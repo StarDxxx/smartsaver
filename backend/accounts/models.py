2e8d84424569da20607b8f813662980dd4082630
Douglas Harrington Muhone <doug@dhmgroup.net>
2022-05-20 08:38:37 +0200

    create user model

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -2,15 +2,57 @@ from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, Permis
 from django.db import models
 
 
+class UserManager(BaseUserManager):
+    def create_user(self, email, name, password=None):
+        """
+        Creates and saves a User with the given email, name and password.
+        """
+        if not email:
+            raise ValueError('Users must have an email address')
+        if not name:
+            raise ValueError('Users must have a name')
+
+        user = self.model(
+            email=self.normalize_email(email),
+            name=name,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, name, password=None):
+        """
+        Creates and saves a superuser with the given email, name and password.
+        """
+        user = self.create_user(
+            email,
+            password=password,
+            name=name,
+        )
+        user.is_superuser = True
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
 class User(AbstractBaseUser, PermissionsMixin):
     name = models.CharField(max_length=150)
     email = models.EmailField(unique=True)
     friends = models.ManyToManyField('self')
+    is_admin = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    objects = UserManager()
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['name']
 
     def __str__(self):
         return self.name
+
+    @property
+    def is_staff(self):
+        return self.is_admin
